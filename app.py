1ec2002db0e832d7c6f084777891bb506a22cb72
sagarsen78 <sagarsen78@gmail.com>
2021-07-05 22:34:40 +0530

    Made a few changes in Flask framework

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
-from flask import Flask,render_template,request
-from flask_cors import cross_origin
-import numpy as np 
+from flask import Flask, render_template,request
+import pickle
+import numpy as np
+
+model1=pickle.load(open('RandomForest.pkl','rb'))
 
 app=Flask(__name__)
 
-#loading the model
-import pickle
-model=pickle.load(open(r'C:\Users\sagar\OneDrive\Desktop\crop\RandomForest.pkl','rb'))
 @app.route('/')
-def home():
+def man():
     return render_template('home.html')
 
-@app.route('/predict',methods=['GET','POST'])
-@cross_origin()
-def predict():
-    if predict.method=='POST':
-        nmeasure=int(request.form['n'])
-        pmeasure=int(request.form['p'])
-        kmeasure=int(request.form['k'])
-        tempmeasure=int(request.form['temp'])
-        humimeasure=int(request.form['humi'])
-        phmeasure=int(request.form['ph'])
-        rainmeasure=int(request.form['rain'])
-        yhat=model.predict([[nmeasure,pmeasure,kmeasure,tempmeasure,humimeasure,phmeasure,rainmeasure]])
-        return render_template('output.html',prediction=yhat[0])
+
+@app.route('/predict',methods=['POST'])
+def home():
+
+    nmeasure = int(request.form['n'])
+    pmeasure = int(request.form['p'])
+    kmeasure = int(request.form['k'])
+    tempmeasure = int(request.form['temp'])
+    humimeasure = int(request.form['humi'])
+    phmeasure = int(request.form['ph'])
+    rainmeasure = int(request.form['rain'])
+
+
+    arr=np.array([[nmeasure,pmeasure,kmeasure,tempmeasure,humimeasure,phmeasure,rainmeasure]])
+    pred=model1.predict(arr)
 
 if __name__=='__main__':
     app.run(debug=True)
